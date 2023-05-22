5c64d4c5141ce1a254eae260c601efa2f946a76a
uoch <chaouch.thameurtc61@gmail.com>
2023-05-22 17:02:50 +0100

    setup

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-apt-get update 
-apt-get install -y hugo make
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
-apt-get install ./hugo.deb
-dpkg -s hugo make
+
+sudo apt-get update && sudo apt-get install -y make git
+
+# download/install minimal version to use template ananke
+sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
+sudo apt install ./hugo.deb
+#remove file after installation
+sudo rm hugo.deb
+
+# download/install linter
 sudo go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.52.2
 sudo npm install -g markdownlint-cli
 sudo npm install -g markdown-link-check
-make build
