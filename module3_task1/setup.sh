c5de640f4bbdef8137befc062e3638a34d62447a
uoch <chaouch.thameurtc61@gmail.com>
2023-05-22 16:58:19 +0100

    setup

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -4,4 +4,7 @@ apt-get install -y hugo make
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
 apt-get install ./hugo.deb
 dpkg -s hugo make
+sudo go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.52.2
+sudo npm install -g markdownlint-cli
+sudo npm install -g markdown-link-check
 make build
