c5f70349ad512e7fa4edcac9a8a446ba5b620e40
uoch <chaouch.thameurtc61@gmail.com>
2023-05-25 19:23:29 +0100

    which

--- module4_task2/setup.sh
+++ module4_task2/setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-export PATH=$PATH:/usr/local/go/bin
-
 rm -rf /usr/local/go
 curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
 sudo apt-get install ./install_hugo.deb
 
 sudo apt update
 sudo apt install build-essential
+sudo apt-get install gcc
+export PATH=$PATH:/usr/local/go/bin:/usr/bin/gcc
+which gcc
